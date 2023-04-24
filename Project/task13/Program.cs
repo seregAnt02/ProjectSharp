b60002c6b1c3b0d9c445e5f5c20421ee10a55e7a
seregAnt02 <129961993+seregAnt02@users.noreply.github.com>
2023-04-24 18:40:37 +0500

    Добавление кода к задаче №13

---
+++ Project/task13/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет."); 
+Console.WriteLine("Введите число: ");
+
+var number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 100) Console.WriteLine("третьей цифры нет");
+
+if(number > 100 && number < 1000)
+    {        
+        var result = number % 10;
+
+        Console.WriteLine($"третья цифра: {result}");
+    }
+
+if (number >= 1000)
+    {
+
+        while (number >= 1000)
+        {
+            number /= 10;
+        }
+
+        var result = number % 10;
+
+        Console.WriteLine($"третья цифра: {result}");
+    }    
