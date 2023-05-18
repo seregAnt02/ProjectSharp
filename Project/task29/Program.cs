9bd2c83918ec26dd9f4b4aa3dbf84bb956f5160b
seregAnt02 <129961993+seregAnt02@users.noreply.github.com>
2023-05-18 18:36:40 +0500

    Загрузка файла проекта к уроку 29

---
+++ Project/task29/Program.cs
@@ -0,0 +1,31 @@
+﻿
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+int size = ReadNamber("Введите размер массива: ");
+int[] array = new int[size];
+PrintArray(array);
+
+//-------------------------------
+//-------------------------------
+// Функция ввода
+int ReadNamber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+//-------------------------------
+// Присваивание случайного значения и вывод массива в консоль.
+void PrintArray(int[] array)
+{
+    for (int x = 0; x < array.Length; x++)
+    {
+        array[x] = new Random().Next(0, 9);
+        Console.Write(array[x]);
+    }
+}
