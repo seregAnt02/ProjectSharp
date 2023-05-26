53bac3f2a86fefcd2c7c4548a47481730b27b944
seregAnt02 <129961993+seregAnt02@users.noreply.github.com>
2023-05-26 18:30:13 +0500

    Update Program.cs

--- Project/task41/Program.cs
+++ Project/task41/Program.cs
@@ -1,1 +1,35 @@
 
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+//-------------------------------
+//-------------------------------
+
+int arrayLength = ReadNamber("Введите размер массива: ");
+int[] ints = new int[arrayLength];
+AssigningValue(ints);
+//-------------------------------
+//Присваивание значения в массиву.
+//Чисел > 0
+void AssigningValue(int[] array)
+{
+    int count = 0;
+    for(int x = 0; x < array.Length; x++)
+    {
+        int number = ReadNamber($"Введите число: { x + 1 } ");
+        array[x] = number;
+
+        if (number > 0) count++;
+    }
+    Console.WriteLine($"Чисел больше 0 ввёл пользователь: {count}");
+}
+//-------------------------------
+// Функция ввода
+int ReadNamber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
