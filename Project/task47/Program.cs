b0fe0b29a56516ee722ed3cdc803bcd0284baf19
seregAnt02 <129961993+seregAnt02@users.noreply.github.com>
2023-05-29 18:55:54 +0500

    Загрузка кода к уроку №47

--- Project/task47/Program.cs
+++ Project/task47/Program.cs
@@ -1,1 +1,42 @@
 
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+
+//-------------------------------
+// Функция ввода
+int ReadNamber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+//---------------------------------
+void CreateArray(double[,] array)
+{    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        string temp = "";
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(new Random().NextDouble(), 1);            
+            temp += array[i, j] + " ";            
+        }
+        Console.WriteLine(temp + "\r\n");
+    }
+}
+//---------------------------------
+//---------------------------------
+
+int row = ReadNamber("Введите размер массива строк: ");
+int column = ReadNamber("Введите размер массива столбцов: ");
+
+double[,] array = new double[row, column];
+
+CreateArray(array);
